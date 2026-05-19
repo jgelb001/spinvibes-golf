dacab43b3683772fe033e7596c005fe6cc0487a6
Jeremy G <jeremy.gelbaum@gmail.com>
2026-05-18 23:03:06 -0700

    Update 2026-05-18 23:03

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-// SpinVibes Golf — Service Worker v33
+// SpinVibes Golf — Service Worker v34
 // Strategy: network-first for HTML, cache-first for everything else
 
-const CACHE = 'spinvibes-golf-v78';
+const CACHE = 'spinvibes-golf-v79';
 
 const ASSETS = [
   '/',
