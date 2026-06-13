edcc3ea9cae79908aa49becd290264e828cd3b67
Jeremy Gelbaum <jeremy.gelbaum@gmail.com>
2026-06-13 06:52:00 -0700

    Update PWA 2026-06-13 06:51

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-// SpinVibes Golf — Service Worker v107
+// SpinVibes Golf — Service Worker v108
 // Strategy: network-first for HTML, cache-first for everything else
 
-const CACHE = 'spinvibes-golf-v153';
+const CACHE = 'spinvibes-golf-v154';
 
 const ASSETS = [
   '/',
